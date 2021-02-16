a4455a4db009a7f84979e6d7e37111cf3b97a2ee
FabioCruz20 <fabiovict95@gmail.com>
2021-02-16 18:00:08 -0300

    Botão de deletar exercício (soft delete) e ajustes no CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useContext, useState, useMemo } from 'react';
 import './Card.css';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Delete from '@material-ui/icons/Delete';
 import { Link } from "react-router-dom";
 
 import fire from '../services/fire';
 import { AuthContext } from "../services/auth";
 import api from '../services/api';
+import SimpleConfirmationDialog from './SimpleConfirmationDialog';
 
 
 const Card = props => {
     const { currentUser } = useContext(AuthContext);
     const [exercises, setExercises] = useState([]);
     const [user, setUser] = useState([]);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [chosenExercise, setChosenExercise] = useState(null);
+
     useEffect(() => {
         const fetchData = async () => {
             const db = fire.firestore();
@@ -36,6 +41,35 @@ const Card = props => {
         props.user ? fetchData() : setExercises([]); setUser([])
     }, [props.user]);
 
+    /**
+     * Chamada ao firestore para fazer o soft delete do exercício, definindo o campo
+     * active como false.
+     * @param {object} exercise exercício a ser excluído
+     */
+    const deleteExercise = async (exercise) => {
+        
+        try {
+            exercise.active = false;
+            let now = new Date();
+
+            const db = fire.firestore();
+            await db.collection('exercises')
+                .doc(exercise.id)
+                .update({
+                    active: exercise.active, 
+                    updatedAt: now, 
+                    deletedAt: now,
+                    deletedBy: currentUser.uid,
+                });
+    
+            setExercises([...exercises]);
+        }
+        catch (e) {
+            alert('Erro: documento não encontrado');
+        }
+
+    };
+
     return (
         <div>
             {user.map(us =>
@@ -50,7 +84,7 @@ const Card = props => {
                         .filter(exercise => exercise.userId === us.userId)
                         .filter((este, i) => exercises.indexOf(este) === i)
                         .map((exercise) => (
-                            <div>
+                            <div key={exercise.id}>
                                 <div class="activityArea">
                                     <div class="containertitle">
                                         <div class="titleActivityArea">
@@ -59,7 +93,22 @@ const Card = props => {
                                                 Início: {new Date(exercise.startDate._seconds * 1000).toLocaleDateString("pt-BR") + " " + new Date(exercise.startDate._seconds * 1000).toTimeString().slice(0, 5)
                                                 } Fim: {new Date(exercise.endDate._seconds * 1000).toLocaleDateString("pt-BR") + " " + new Date(exercise.endDate._seconds * 1000).toTimeString().slice(0, 5)
                                                 }</p>
+                                            <p 
+                                                className={`activeStatus ${exercise.active ? 'active' : 'inactive'}`}>
+                                                {exercise.active ? 'Ativo' : 'Inativo'}
+                                            </p>
                                         </div>
+                                        
+                                        <button 
+                                            className="btnDelete"
+                                            disabled={!exercise.active || exercise.currentlyUsed}
+                                            onClick={(e) =>  {
+                                                    setChosenExercise(exercise);
+                                                    setDialogOpen(true);
+                                                }
+                                            }>
+                                            <Delete fontSize="small"></Delete>
+                                        </button>
                                     </div>
                                     <div class="activity">
                                         {/* <div>{'Tamanho do exercise.activities '+exercise.activities.length}</div> */}
@@ -104,6 +153,21 @@ const Card = props => {
                     }
                 </div >
             )}
+            <SimpleConfirmationDialog 
+                open={dialogOpen}
+                title="Desativar exercício"
+                message="Tem certeza que deseja desativar o exercício?"
+                cancelText="Não"
+                confirmText="Sim"
+                onConfirm={() => {
+                    deleteExercise(chosenExercise);
+                    setChosenExercise(null);
+                    setDialogOpen(false);
+                }}
+                onClose={() => {
+                    setChosenExercise(null);
+                    setDialogOpen(false);
+                }}/>
         </div>
 
     );
